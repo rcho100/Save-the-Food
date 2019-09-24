44a879ebb344380ae417dd1dd9461b9e1a00f163
github username <github email address>
2019-09-23 21:18:39 -0400

    Generate resource for Ingredient and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_224912) do
+ActiveRecord::Schema.define(version: 2019_09_24_011609) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "measurements"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
