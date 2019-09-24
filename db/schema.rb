3bd86c5749a9792c2b87f2645d4d0381fc842e52
github username <github email address>
2019-09-24 13:31:41 -0400

    Add foreign key to Recipes table and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_170758) do
+ActiveRecord::Schema.define(version: 2019_09_24_171413) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_09_24_170758) do
     t.text "directions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
