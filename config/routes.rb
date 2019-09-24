5dd39fc4e50e5866bc10e3e7e886bf5dad995eb9
github username <github email address>
2019-09-24 15:43:15 -0400

    Add custom routes for login and signup and modify resources for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root "sessions#welcome"
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+
   resources :reviews
   resources :ingredients
   resources :recipes
-  resources :users
+  resources :users, only: [:create, :edit, :show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
