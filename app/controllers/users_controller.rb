0e949d7fc6affb9c7beb75d2c248e3689614bb14
github username <github email address>
2019-09-24 21:57:26 -0400

    Add create action in UsersController w/ conditional logic inside

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
 
   private
 
