33b61abdc27dcdb69980d2894f404426419bcf5f
github username <github email address>
2019-09-24 23:17:39 -0400

    Add email to Users table & migrate to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_171413) do
+ActiveRecord::Schema.define(version: 2019_09_25_031533) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_171413) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
