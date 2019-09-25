ad1cfa28d1f8fddb00d6dca11ccfa6577b8842c0
github username <github email address>
2019-09-25 02:26:41 -0400

    Add custom route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
 
   resources :reviews
