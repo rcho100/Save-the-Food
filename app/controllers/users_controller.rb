f94b2aa74b6571238d3b3db03a8bfc2e4a5478b8
github username <github email address>
2019-09-24 23:26:52 -0400

    Add email fields to login and signup forms & add email to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :email)
   end
 
 end
