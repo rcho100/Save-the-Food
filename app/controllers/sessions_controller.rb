26d40cc93fb16a55363c3e7dbf090c2a4a6b7cd1
github username <github email address>
2019-09-24 22:53:33 -0400

    Add create action to log user in and then redirect to user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,13 @@ class SessionsController < ApplicationController
   def new
     
   end
+
+  def create
+    user = User.find_by(name: params[:name])
+    user.authenticate(params[:password])
+
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+
+  end
 end
