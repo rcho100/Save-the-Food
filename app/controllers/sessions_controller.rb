65d39b51a86d9d325dfe2af0976dcc6afe858cfb
github username <github email address>
2019-09-24 23:05:00 -0400

    Add conditional logic for the case where user can't be found with given data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
-    user.authenticate(params[:password])
+    if user = User.find_by(name: params[:name])
+      user.authenticate(params[:password])
 
-    session[:user_id] = user.id
-    redirect_to user_path(user)
-
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to root_path
+    end
   end
 end
