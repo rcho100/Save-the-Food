43c3af010e561692c968a7c02f0434340ff1f0da
github username <github email address>
2019-09-25 02:11:18 -0400

    Add validations to User Model and modify create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    if @user = User.new(user_params)
+    @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :reviewed_recipes, through: :reviews, source: :recipe
   has_secure_password
+
+  validates :name, format: { without: /[0-9]/, message: "does not allow numbers" }
+  validates :email, uniqueness: true
 end
