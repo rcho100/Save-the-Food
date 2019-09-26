4f951821bdb80d70214af5380d3b15cc5f7e84b5
github username <github email address>
2019-09-26 14:51:53 -0400

    Add code to clear database and add dummy data for User objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+##############################
+
+# create_table "ingredients", force: :cascade do |t|
+#     t.string "name"
+#     t.string "measurements"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+#   create_table "recipe_ingredient_lists", force: :cascade do |t|
+#     t.integer "ingredient_id"
+#     t.integer "recipe_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.index ["ingredient_id"], name: "index_recipe_ingredient_lists_on_ingredient_id"
+#     t.index ["recipe_id"], name: "index_recipe_ingredient_lists_on_recipe_id"
+#   end
+
+#   create_table "recipes", force: :cascade do |t|
+#     t.string "name"
+#     t.integer "servings"
+#     t.integer "time"
+#     t.text "directions"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.integer "user_id"
+#     t.index ["user_id"], name: "index_recipes_on_user_id"
+#   end
+
+#   create_table "reviews", force: :cascade do |t|
+#     t.string "name"
+#     t.integer "rating"
+#     t.text "content"
+#     t.integer "user_id"
+#     t.integer "recipe_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
+#     t.index ["user_id"], name: "index_reviews_on_user_id"
+#   end
+
+#   create_table "users", force: :cascade do |t|
+#     t.string "name"
+#     t.string "password_digest"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.string "email"
+#   end
+
+##############################
+
+Ingredient.destroy_all
+Recipe.destroy_all
+Review.destroy_all
+User.destroy_all
+
+#Users
+3.times do
+  User.create!(
+    name: Faker::Name.name ,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password 
+  )
+end
+puts "#{User.all.count} users created"
+
