25671a2bbad9578d767bc1ce8b9094a5ee92b070
github username <github email address>
2019-09-26 14:06:23 -0400

    Update Recipe and Ingredient Models w/ updated associations through a join table

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredient_lists
+  has_many :recipes, through: :recipe_ingredient_lists
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
-  has_many :ingredients
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
+  has_many :recipe_ingredient_lists
+  has_many :ingredients, through: :recipe_ingredient_lists
 end
