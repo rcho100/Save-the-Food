1195d70f17df23b46fa6d10139b4fb3d0428f06f
github username <github email address>
2019-09-27 15:56:06 -0400

    Add create action & recipe_params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,21 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+ 
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :servings, :time, :directions, ingredient_ids: [])
+  end
+
 end
