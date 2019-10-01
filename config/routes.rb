76e5efcbcc41943f52753b1194cf2534743ba682
Richard Cho <richardcho100@gmail.com>
2019-10-01 02:03:30 -0400

    Nest reviews resources in recipes resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   resources :ingredients
-  resources :recipes
+  resources :recipes do
+    resources :reviews
+  end
   resources :users, only: [:create, :edit, :show, :update] do
     resources :reviews
   end
