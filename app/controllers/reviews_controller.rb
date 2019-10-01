c0438e1a6a8bfb4305d68485100876bf234fa89c
Richard Cho <richardcho100@gmail.com>
2019-10-01 00:41:47 -0400

    Create index action that handles both regular & nested routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+  def index
+    if params[:user_id]
+      @reviews = User.find(params[:user_id]).reviews
+    else
+      @reviews = Review.all
+    end
+  end
 end
