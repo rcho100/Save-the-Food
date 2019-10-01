5ded30e39f4af6c650955092c9c122f5bbb94dcc
Richard Cho <richardcho100@gmail.com>
2019-10-01 03:23:52 -0400

    Modify with a little stylistic change in written code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def current_user
-    current_user ||= User.find_by(id: session[:user_id])
+    current_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
-    current_user
+    !!current_user
   end
 end
