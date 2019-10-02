531b217b44f08a3f7e32fb837c8009bbb45c3bd0
Richard Cho <richardcho100@gmail.com>
2019-10-02 17:26:19 -0400

    Add validations for Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredient_lists
   has_many :recipes, through: :recipe_ingredient_lists
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
 end
