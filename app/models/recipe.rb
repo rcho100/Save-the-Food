4c8b41f9bcf2b734a7e9ffb0212181f97f3b4270
Richard Cho <richardcho100@gmail.com>
2019-10-02 18:21:13 -0400

    Add validations for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,9 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredient_lists
   accepts_nested_attributes_for :ingredients 
 
+  validates :name, :servings, :time, :directions, presence: true
+  validates :name, uniqueness: true
+  validates :servings, :time, numericality: { greater_than_or_equal_to: 1 }
+
   scope :top_three, -> { joins(:reviews).group(:id).order(rating: :desc).limit(3) }
 end
