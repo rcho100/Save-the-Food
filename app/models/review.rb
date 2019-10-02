8a18799e721a9c226d44369d4abbf139d5621e9b
Richard Cho <richardcho100@gmail.com>
2019-10-02 18:51:06 -0400

    Add validations for Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
