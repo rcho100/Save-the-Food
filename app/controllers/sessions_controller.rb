0ed8d955a778f92c7516097fcd922de03ed88eff
Richard Cho <richardcho100@gmail.com>
2019-10-03 19:01:40 -0400

    Add private #auth & omniauth action to log in user via OmniAuth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,4 +23,17 @@ class SessionsController < ApplicationController
     session.delete :user_id
     redirect_to root_path
   end
+
+  def omniauth
+    @user = User.from_omniauth(auth)
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user), success: 'Success! You are now logged in'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
