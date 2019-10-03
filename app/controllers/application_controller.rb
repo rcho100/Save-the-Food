5669063abc8d74ef997a08343f2df515349ba144
Richard Cho <richardcho100@gmail.com>
2019-10-03 03:24:12 -0400

    Ensure user is assigned to var only if session[:user_id] exists & change local var to instance var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info
 
   def current_user
-    current_user ||= User.find_by_id(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
