76b2c1d6eb6797c4a020927402b281236c8aef5a
Richard Cho <richardcho100@gmail.com>
2019-10-05 01:57:30 -0400

    Add #ingredients_attributes - make sure duplicate ingredients are retrieved & not saved

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,5 +10,11 @@ class Recipe < ApplicationRecord
   validates :name, uniqueness: true
   validates :servings, :time, numericality: { greater_than_or_equal_to: 1 }
 
+  def ingredients_attributes=(attributes)
+    attributes.values.each do |value|
+      self.ingredients << Ingredient.find_or_create_by(value) if value['name'].present?
+    end
+  end
+
   scope :top_three, -> { joins(:reviews).group(:id).order(rating: :desc).limit(3) }
 end
