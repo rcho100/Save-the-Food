7a598c7abe537f8ad982a2ca749148f2bc48077e
Richard Cho <richardcho100@gmail.com>
2019-10-05 16:33:50 -0400

    Delete index action & index view page along w/ the helper method used in the view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,5 @@
 class ReviewsController < ApplicationController
   before_action :require_login
-  
-  def index
-    if params[:user_id]
-      @user = User.find(params[:user_id])
-      @reviews = @user.reviews
-    else
-      @reviews = Review.all
-    end
-  end
 
   def new
     @review = Review.new(recipe_id: params[:recipe_id], user_id: current_user.id)
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,10 +1,2 @@
 module ReviewsHelper
-
-  def index_heading_for_reviews
-    if !!@user
-      "#{@user.name}'s Reviews"
-    else
-      "All Reviews"
-    end
-  end
 end
