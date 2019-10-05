c0e5794c32a090104b135f692c8902e05416e9e0
Richard Cho <richardcho100@gmail.com>
2019-10-05 00:46:53 -0400

    Refactor validates in Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,5 @@ class Review < ApplicationRecord
   belongs_to :recipe
 
   validates :rating, :content, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 1 }
-  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
