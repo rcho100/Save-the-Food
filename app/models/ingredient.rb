adffe8747bfcc5ca2fdb927d54593871e9a550c3
Richard Cho <richardcho100@gmail.com>
2019-10-05 02:43:54 -0400

    Delete validations for Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,4 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredient_lists
   has_many :recipes, through: :recipe_ingredient_lists
-
-  validates :name, presence: true
-  validates :name, uniqueness: true
-
 end
