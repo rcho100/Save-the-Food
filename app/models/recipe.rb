f253126a104ec015a47eab9074c2e46fc8bf9ebc
Richard Cho <richardcho100@gmail.com>
2019-10-07 21:40:58 -0400

    Add scope #under_thirty to find Recipes that take under 30mins

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,6 +10,8 @@ class Recipe < ApplicationRecord
   validates :name, uniqueness: true
   validates :servings, :time, numericality: { greater_than_or_equal_to: 1 }
 
+  scope :under_thirty, -> { where("time < 30") }
+
   def ingredients_attributes=(attributes)
     attributes.values.each do |value|
       self.ingredients << Ingredient.find_or_create_by(value) if value['name'].present?
