072b1093fc5535b539b1ee927a930be8b1f9fcbe
Richard Cho <richardcho100@gmail.com>
2019-10-09 19:54:06 -0400

    Add three_stars class method to get reviews over 3 stars given by a user

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
 
   validates :rating, :content, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def self.three_stars(user_id)
+    joins(:user).where("rating > 3").where("user_id == ?", user_id)
+  end
 end
